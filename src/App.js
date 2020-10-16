a2403780d02c28a0b9e04ae40764bacf7bc0fded
Ayuba Olanrewaju <Babarindeolanrewaju@gmail.com>
2020-10-16 18:44:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// state, this.setState = hold/access/change data of component
+// props = read only
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import Booklist from "./Booklist";
+import "./App.css";
+const App = () => (
+  <section>
+    <Booklist />
+  </section>
+);
 
 export default App;
